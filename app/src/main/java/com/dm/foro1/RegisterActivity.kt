8cd160469c56922dedefb28a6bb8ae5e4cd5e2f6
ivanboot <ivanma.garcia@hotmail.com>
2024-04-28 22:43:49 -0600

    Integrando carrito con clases bd

--- app/src/main/java/com/dm/foro1/RegisterActivity.kt
+++ app/src/main/java/com/dm/foro1/RegisterActivity.kt
@@ -1,10 +1,7 @@
 package com.dm.foro1
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 import android.content.Intent
 import android.database.sqlite.SQLiteDatabase
 import android.widget.Button
